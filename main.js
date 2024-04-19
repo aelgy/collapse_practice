1fcc793e38ef3f3fe3c64fbded536dd40c7684ca
aelgy <aelgy333@gmail.com>
2024-04-20 06:48:26 +0800

    add question background

--- main.js
+++ main.js
@@ -1,26 +1,16 @@
 function collapse() {
-  const toggle = event.target.nextElementSibling;
-  if (toggle.classList.contains('slide-down')) {
-    toggle.classList.remove('slide-down');
-    toggle.classList.add('slide-up')
-  } else if (toggle.classList.contains('slide-up')) {
-    toggle.classList.remove('slide-up');
-    toggle.classList.add('slide-down');
-  } else {
-    toggle.classList.add('slide-down');
-  }
-}
-
-function collapseI() {
+  const color = event.target.closest("div")
   const toggle = event.target.closest("div").nextElementSibling;
-  const add = event.target;
+  const add = event.target.closest("div").lastElementChild
   if (toggle.classList.contains('slide-down')) {
     toggle.classList.remove('slide-down');
-    toggle.classList.add('slide-up')
+    toggle.classList.add('slide-up');
+    add.classList.remove('transform');
+    color.classList.remove('color');
   } else if (toggle.classList.contains('slide-up')) {
     toggle.classList.remove('slide-up');
     toggle.classList.add('slide-down');
-  } else {
-    toggle.classList.add('slide-down');
+    add.classList.add('transform');
+    color.classList.add('color');
   }
 }
